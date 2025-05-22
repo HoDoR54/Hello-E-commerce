c83c46c1e6a1a07a7139f220905c241de5e83b50
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2025-05-22 22:43:25 +0630

    [Feature] Customer login

--- Hello E-commerce Backend/Controllers/AuthController.cs
+++ Hello E-commerce Backend/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using E_commerce_Admin_Dashboard.DTO.Requests;
 using E_commerce_Admin_Dashboard.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_commerce_Admin_Dashboard.Controllers
@@ -14,10 +15,21 @@ namespace E_commerce_Admin_Dashboard.Controllers
             _authServices = authServices;
         }
 
-        [HttpPost("/admin/login")]
-        public async Task<IActionResult> AdminLogIn([FromBody] LoginRequest req)
+        [HttpPost("/admins/login")]
+        public async Task<IActionResult> AdminLogin([FromBody] LoginRequest req)
         {
             var response = await _authServices.AdminLoginAsync(req);
+
+            if (response == null)
+                return Unauthorized("Invalid email or password.");
+
+            return Ok(response);
+        }
+
+        [HttpPost("/customers/login")]
+        public async Task<IActionResult> CustomerLogin([FromBody] LoginRequest req)
+        {
+            var response = await _authServices.CustomerLoginAsync(req);
 
             if (response == null)
                 return Unauthorized("Invalid email or password.");
