01b0bc5e4c70aba5702d19f4cc62544ca6a2d293
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2025-06-19 21:46:16 +0630

    configure backend to allow frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -11,11 +11,22 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .AllowCredentials();
+        });
+});
+
+// Add services to the container.
 
 builder.Services.AddHttpContextAccessor();
 
@@ -53,6 +64,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
